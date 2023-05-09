94843a716a99a109045255875477a02a904964f3
mattycarel <joemattycarelb@gmail.com>
2023-05-09 10:26:29 +0200

    add list Months

---
+++ main.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    Months=[
+        "Janvier" ,
+        "Fevrier",
+        "Mars",
+        "Avril",
+        "Mai",
+        "Juin",
+        "Juillet",
+        "Aout",
+        "Septembre",
+        "Octobre"    
+    ]
