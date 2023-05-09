2ebf481f06e8347110f21eeccd1abac555ac94ab
mattycarel <joemattycarelb@gmail.com>
2023-05-09 13:02:59 +0200

    add dictionaire Person

--- main.py
+++ main.py
@@ -115,9 +115,13 @@ if __name__ == '__main__':
     
     print("------Supprimer le set------")
     del Players
-    print(Players)
+    
     
     print("----QUESTION IV-----")
+    
+    Person={
+        
+    }
     
           
     
